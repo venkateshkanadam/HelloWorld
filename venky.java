33fcb6eb595c703b08d5746bedcac65bbbb58d7d
venkateshkanadam <159518813+venkateshkanadam@users.noreply.github.com>
2024-02-09 20:37:59 +0530

    Update venky.java
    updated

--- venky.java
+++ venky.java
@@ -2,9 +2,9 @@ class venky
 {
 	public static void main(String args[])
 	{
-		 int a =10;
-		 int b =20;
-		 int c =a+b;
-		 System.out.println("The sum is:"+c);
+		 int a = 10;
+		 int b = 20;
+		 int c = a+b;
+		 System.out.println("The a and b sum is:"+c);
 	}
 }
